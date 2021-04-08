c219695ed2e9da80801dffc9d2724830b58a8523
Yuanhong Xiao <yuanhong@seas.upenn.edu>
2021-04-08 21:19:48 +0000

    after milestone 1 meeting

--- helper.h
+++ helper.h
@@ -2,6 +2,7 @@
 #include "voter.h"
 #include <iostream>
 #include <vector>
+#include <unordered_set>
 #include <string>
 #include <cstring>
 #include <stdio.h>
@@ -22,7 +23,7 @@ extern int running_thread;
 extern string password;
 extern vector<Candidate *> candidates;
 extern vector<Voter *> voters;
-extern vector<int> generatedValues;
+extern unordered_set<int> generatedMagicNumbers;
 // flag
 extern bool isOngoing;
 extern int highest_vote;
@@ -30,20 +31,20 @@ extern bool changePassword;
 // main() & mutex
 extern string userCmds[];
 extern pthread_mutex_t parseLock;
+extern pthread_mutex_t runningThreadLock;
 // extern pthread_mutex_t userCmdsLock;
 // extern pthread_mutex_t candidatesLock;
 // extern pthread_mutex_t votersLock;
-extern pthread_mutex_t methodLock;
 // extern pthread_mutex_t inputLock;
 // extern pthread_mutex_t magicNumLock;
 
 // helper function
 void view_result_helper();
 bool isNumber(const string &str);
-int generateUniqueInt();
+int generateUniqueMagicNumber();
 
 // PARSER //
-std::vector<std::string> parseCmd(const std::string &raw_line, const std::string &delim);
+vector<string> parseCmd(const string &raw_line, const string &delim);
 
 //ADMIN
 void start_election(string cmdpassword);
